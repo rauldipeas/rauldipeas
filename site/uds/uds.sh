cc974e6589becaaac78c3e6c799d2aaaaafa7e38
Raul Dipeas <github@rauldipeas.com.br>
2025-07-10 21:33:15 -0300

    ^uds

--- site/uds/uds.sh
+++ site/uds/uds.sh
@@ -103,7 +103,7 @@ for nome in "${sel_arr[@]}"; do
     if [[ "${entries[i]}" == "$nome" ]]; then
       script_url="https://rauldipeas.com;br/uds/$script_path"
       echo "Executando $nome..."
-      xterm -T "$nome" -fa 'Monospace' -fs 11 -bg darkblue -fg white -e bash -c "bash <(curl -fsSL '$script_url'); echo 'Pressione qualquer tecla para fechar...'; read -n1"
+      xterm -T "$nome" -fa 'Monospace' -fs 11 -bg darkblue -fg white -e bash -c "bash <(wget -qO- '$script_url'); echo 'Pressione qualquer tecla para fechar...'; read -n1"
     fi
   done
 done
