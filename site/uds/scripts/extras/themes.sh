029f8fb9ed8dc7343c164d6f144a9bcc11f6b8cf
Raul Dipeas <github@rauldipeas.com.br>
2025-07-14 22:39:09 -0300

    ^uds

--- site/uds/scripts/extras/themes.sh
+++ site/uds/scripts/extras/themes.sh
@@ -47,6 +47,8 @@ dconf write /org/gnome/shell/extensions/alphabetical-app-grid/sort-folder-conten
 dconf write /org/gnome/shell/extensions/app-hider/hidden-apps "['display-im6.q16.desktop', 'micro.desktop', 'RealTimeSync.desktop', 'syncthing-start.desktop', 'syncthing-ui.desktop', 'debian-uxterm.desktop', 'debian-xterm.desktop', 'winetricks.desktop', 'diodon.desktop', 'synaptic.desktop', 'superpaper.desktop']"
 dconf write /org/gnome/shell/extensions/appindicator/custom-icons "[('vlc', 'vlc-panel', ''), ('deltachat-desktop', 'deltachat-tray', ''), ('rambox', 'rambox-indicator', ''), ('superpaper', 'preferences-desktop-display-randr', ''), ('Diodon', 'notes-panel', ''), ('QjackCtl', 'gnome-device-manager', ''), ('rclone-browser', 'cloudstatus', ''), ('2wydifuftb', 'gtk-dialog-authentication-panel', ''), ('un-reboot', 'system-reboot-symbolic', ''), ('q4wine', 'folder-white-wine', '')]"
 dconf reset -f /org/gnome/shell/extensions/blur-my-shell
+dconf write /org/gnome/shell/extensions/blur-my-shell/applications/blur true
+dconf write /org/gnome/shell/extensions/blur-my-shell/applications/whitelist "['com.mitchellh.ghostty']"
 dconf write /org/gnome/shell/extensions/blur-my-shell/dash-to-dock/override-background true
 dconf write /org/gnome/shell/extensions/blur-my-shell/dash-to-dock/unblur-in-overview true
 dconf write /org/gnome/shell/extensions/blur-my-shell/panel/override-background-dynamically true
