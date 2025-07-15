307cb8d7d65556d659d8aea89e210dd774de5c83
Raul Dipeas <github@rauldipeas.com.br>
2025-07-15 17:18:39 -0300

    ^uds

--- site/uds/functions.sh
+++ site/uds/functions.sh
@@ -10,8 +10,8 @@ download(){
 }
 
 fix_launcher() {
-    sudo rm -f /usr/local/share/custom-launchers/$(basename ${BASH_SOURCE[1]}|cut -d '.' -f1)
-    cat <<EOF |sudo tee /usr/local/share/custom-launchers/$(basename ${BASH_SOURCE[1]}|cut -d '.' -f1)>/dev/null
+    sudo rm -f /usr/local/share/custom-launchers/$(basename $script_url|cut -d '.' -f1)
+    cat <<EOF |sudo tee /usr/local/share/custom-launchers/$(basename $script_url|cut -d '.' -f1)>/dev/null
 if [ -f /usr/share/applications/$LN.desktop ] || [ -f /usr/local/share/applications/$LN.desktop ];then
     if [ -f  /usr/share/applications/$LN.desktop ];then
         sed -i 's|Exec=$EXEC_OLD|Exec=$EXEC_NEW|g' /usr/share/applications/$LN.desktop
@@ -67,7 +67,7 @@ install_deb() {
 
 install_appimage() {
     appimage=$(sudo find /tmp -name '*.AppImage')
-    basename=$(basename ${BASH_SOURCE[1]}|cut -d '.' -f1)
+    basename=$(basename $script_url|cut -d '.' -f1)
     chmod +x $appimage
     $appimage --appimage-extract>/dev/null
     sudo rm -rf /opt/$basename
