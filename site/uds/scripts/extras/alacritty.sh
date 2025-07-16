65b6dae869183f3dd9eda3af481f511ace398cca
Raul Dipeas <github@rauldipeas.com.br>
2025-07-16 16:08:46 -0300

    ^uds

---
+++ site/uds/scripts/extras/alacritty.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+set -e
+#LN=''
+#SWMC=''
+#TARGET="$()"
+#EXEC_OLD=''
+#EXEC_NEW=''
+#ICON_OLD=''
+#ICON_NEW=''
+#DEPS=''
+#PPA=''
+INSTNAME='alacritty'
+source <(wget -qO- https://rauldipeas.com.br/uds/functions.sh)
+#enter_tmp
+#download
+#fix_launcher
+install_deb
+#gsettings set org.gnome.desktop.default-applications.terminal exec alacrity
+mkdir -p "$HOME"/.config/alacritty
+cat <<EOF |tee "$HOME"/.config/alacritty/alacritty.toml>/dev/null
+[colors.primary]
+#background = "#00008b"  # darkblue
+background = "#007f20"  # fosforgreen
+foreground = "#ffffff"  # white
+
+[font]
+normal = { family = "Ubuntu Mono", style = "Regular" }
+bold = { family = "Ubuntu Mono", style = "Bold" }
+italic = { family = "Ubuntu Mono", style = "Italic" }
+bold_italic = { family = "Ubuntu Mono", style = "Bold Italic" }
+size = 11.0
+
+[window]
+opacity = 0.6
+#dimensions = { columns = 80, lines = 24 }
+EOF
--- site/uds/scripts/extras/retroarch.sh
+++ site/uds/scripts/extras/retroarch.sh
@@ -3,17 +3,41 @@ set -e
 #TODO
 #LN=''
 #SWMC=''
-#TARGET="$()"
+TARGET="$(wget -qO- 'https://www.retroarch.com/?page=platforms'|grep AppImage|grep RetroArch.7z|cut -d '"' -f2)"
 #EXEC_OLD=''
 #EXEC_NEW=''
 #ICON_OLD=''
 #ICON_NEW=''
 #DEPS=''
-PPA='libretro/stable'
-INSTNAME='retroarch'
+#PPA='libretro/stable'
+#INSTNAME='retroarch'
 source <(wget -qO- https://rauldipeas.com.br/uds/functions.sh)
-#enter_tmp
-#download
+enter_tmp
+download
 #fix_launcher
-add_ppa
-install_deb
+#add_ppa
+#install_deb
+7za x -y -bsp0 RetroArch.7z>/dev/null
+#/tmp/RetroArch-Linux-x86_64/RetroArch-Linux-x86_64.AppImage --appimage-extract
+#sudo mv /tmp/squashfs-root /opt/retroarch
+mkdir -p "$HOME"/.local/share/retroarch
+mv /tmp/RetroArch-Linux-x86_64/RetroArch-Linux-x86_64.AppImage* "$HOME"/.local/share/retroarch/
+ln -fs "$HOME"/.local/share/retroarch/RetroArch-Linux-x86_64.AppImage "$HOME"/.local/bin/retroarch
+cat <<EOF |tee "$HOME"/.local/share/applications/retroarch.desktop>/dev/null
+[Desktop Entry]
+Version=1.0
+Name=RetroArch
+GenericName=Frontend for the libretro API
+Type=Application
+Comment=Frontend for emulators, game engines and media players
+Comment[ru]=Графический интерфейс для эмуляторов, игровых движков и медиаплееров
+Comment[fr]=Interface graphique pour émulateurs, moteurs de jeu et lecteurs multimédia
+Comment[de]=Front-End für Emulatoren, Spiel-Engines und Mediaplayer
+Icon=com.libretro.RetroArch
+Exec=retroarch
+Terminal=false
+StartupNotify=false
+StartupWMClass=retroarch
+Keywords=multi;engine;emulator;xmb;
+Categories=Game;Emulator;
+EOF
