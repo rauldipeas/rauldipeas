f8886214114d2df94f9f35e045069f9dc6ff3e23
Raul Dipeas <github@rauldipeas.com.br>
2025-07-18 00:37:43 -0300

    ^uds

--- site/uds/scripts/image/gimp.sh
+++ site/uds/scripts/image/gimp.sh
@@ -1,25 +1,26 @@
 #!/bin/bash
 set -e
-BASENAME="gimp"
-LN='org.gimp.GIMP.Stable'
+#BASENAME="gimp"
+#LN='org.gimp.GIMP.Stable'
 #SWMC=''
-GIMP_VER="$(wget -qO- https://www.gimp.org/downloads|grep 'stable'|head -n1|cut -d '>' -f3|cut -d '<' -f1)"
-TARGET="https://download.gimp.org/gimp/v3.0/linux/GIMP-$GIMP_VER-x86_64.AppImage"
-EXEC_OLD='org.gimp.GIMP.Stable'
-EXEC_NEW='gimp'
-ICON_OLD='org.gimp.GIMP.Stable'
-ICON_NEW='gimp'
+#GIMP_VER="$(wget -qO- https://www.gimp.org/downloads|grep 'stable'|head -n1|cut -d '>' -f3|cut -d '<' -f1)"
+#TARGET="https://download.gimp.org/gimp/v3.0/linux/GIMP-$GIMP_VER-x86_64.AppImage"
+#EXEC_OLD='org.gimp.GIMP.Stable'
+#EXEC_NEW='gimp'
+#ICON_OLD='org.gimp.GIMP.Stable'
+#ICON_NEW='gimp'
 DEPS="libcanberra-gtk-module\
 	gtk2-engines-murrine\
 	gtk2-engines-pixbuf"
-#PPA=''
-#INSTNAME=''
+PPA='ubuntuhandbook1/gimp-3'
+INSTNAME='gimp'
 source <(wget -qO- https://rauldipeas.com.br/uds/functions.sh)
 #fix_launcher
+#enter_tmp
+#download
+add_ppa
 install_deb
+#install_appimage
 mkdir -p "$HOME"/.config/GIMP/3.0
 wget -q --show-progress -O "$HOME"/.config/GIMP/3.0/gimprc https://rauldipeas.com.br/uds/settings/gimprc
 wget -q --show-progress -O "$HOME"/.config/GIMP/3.0/sessionrc https://rauldipeas.com.br/uds/settings/sessionrc
-enter_tmp
-download
-install_appimage
