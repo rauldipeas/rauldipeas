97750ae7abd13408365546783a1c113ea3a0172e
Raul Dipeas <github@rauldipeas.com.br>
2025-07-21 03:06:25 -0300

    ^uds

--- site/uds/scripts/video/iriun.sh
+++ site/uds/scripts/video/iriun.sh
@@ -17,13 +17,15 @@ download
 fix_launcher
 if grep -E "liquorix|xanmod" <(uname -r);then
     cd /tmp
-    rm -rf /tmp/v4l2loopback
-    git clone https://github.com/v4l2loopback/v4l2loopback.git
-    cd "$PWD"/v4l2loopback
-    make
-    sudo apt install -y checkinstall
-    sudo checkinstall --pkgname=v4l2loopback --pkgversion="$(git describe --tags | sed 's/v//')" --backup=no --deldoc=yes --fstrans=no --default --provides=v4l2loopback-dkms
-    sudo depmod -a
-    sudo modprobe v4l2loopback
+    rm -rf /tmp/v4l2loopback*
+#    git clone https://github.com/v4l2loopback/v4l2loopback.git
+#    cd "$PWD"/v4l2loopback
+#    make
+#    sudo apt install -y checkinstall
+#    sudo checkinstall --pkgname=v4l2loopback --pkgversion="$(git describe --tags | sed 's/v//')" --backup=no --deldoc=yes --fstrans=no --default --provides=v4l2loopback-dkms
+#    sudo depmod -a
+#    sudo modprobe v4l2loopback
+wget -q --show-progress http://mirrors.kernel.org/ubuntu/pool/universe/v/v4l2loopback/"$(wget -qO- http://mirrors.kernel.org/ubuntu/pool/universe/v/v4l2loopback/|grep '.deb"'|grep dkms|grep 0.15|cut -d '"' -f2)"
+sudo apt install -y ./v4l2loopback*.deb
 fi
 install_deb
