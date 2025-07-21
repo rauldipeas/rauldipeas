dca3b2d78282529c7b19fb60261bc76ed76b0aa4
Raul Dipeas <github@rauldipeas.com.br>
2025-07-21 02:31:41 -0300

    ^uds

--- site/uds/scripts/core/cable.sh
+++ site/uds/scripts/core/cable.sh
@@ -16,13 +16,18 @@ enter_tmp
 download
 fix_launcher
 install_deb
-mkdir -p "$HOME"/.config/autostart
+mkdir -p "$HOME"/.config/{autostart,cable}
 cat <<EOF |tee "$HOME"/.config/autostart/cable-autostart.desktop>/dev/null
 [Desktop Entry]
 Type=Application
 Name=Cable
-Exec=env QT_QPA_PLATFORM=xcb pw-jack cable --minimized
+Exec=sh -c "sleep 5;env QT_QPA_PLATFORM=xcb pw-jack cable --minimized"
 Icon=laditools
 Terminal=false
 X-GNOME-Autostart-enabled=true
+EOF
+cat <<EOF |tee "$HOME"/.config/cable/config.ini>/dev/null
+[DEFAULT]
+tray_enabled = True
+autostart_enabled = True
 EOF
