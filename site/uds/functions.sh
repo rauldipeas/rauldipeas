b7e411defba11f61bab4dee7496bd35e85087839
Raul Dipeas <github@rauldipeas.com.br>
2025-07-22 21:54:17 -0300

    ^uds

--- site/uds/functions.sh
+++ site/uds/functions.sh
@@ -14,11 +14,11 @@ fix_launcher() {
     cat <<EOF |sudo tee /usr/local/share/custom-launchers/$BASENAME>/dev/null
 if [ -f /usr/share/applications/$LN.desktop ] || [ -f /usr/local/share/applications/$LN.desktop ];then
     if [ -f  /usr/share/applications/$LN.desktop ];then
-        sed -i 's|Exec=$EXEC_OLD|Exec=$EXEC_NEW|g' /usr/share/applications/$LN.desktop
+        sed -i 's|^Exec=$EXEC_OLD|Exec=$EXEC_NEW|g' /usr/share/applications/$LN.desktop
         sed -i 's|Icon=$ICON_OLD|Icon=$ICON_NEW|g' /usr/share/applications/$LN.desktop
         else
         if [ -f  /usr/local/share/applications/$LN.desktop ];then
-            sed -i 's|Exec=$EXEC_OLD|Exec=$EXEC_NEW|g' /usr/local/share/applications/$LN.desktop
+            sed -i 's|^Exec=$EXEC_OLD|Exec=$EXEC_NEW|g' /usr/local/share/applications/$LN.desktop
             sed -i 's|Icon=$ICON_OLD|Icon=$ICON_NEW|g' /usr/local/share/applications/$LN.desktop    
         fi
     fi
@@ -40,7 +40,7 @@ EOF
     if [ -n "$BASENAME2" ];then
         cat <<EOF |sudo tee /usr/local/share/custom-launchers/$BASENAME2 >/dev/null
 if [ -f /usr/share/applications/$LN2.desktop ];then
-    sed -i 's|Exec=$EXEC_OLD2|Exec=$EXEC_NEW2|g' /usr/share/applications/$LN2.desktop
+    sed -i 's|^Exec=$EXEC_OLD2|Exec=$EXEC_NEW2|g' /usr/share/applications/$LN2.desktop
     sed -i 's|Icon=$ICON_OLD2|Icon=$ICON_NEW2|g' /usr/share/applications/$LN2.desktop
     if ! grep 'StartupWMClass=$SWMC2' /usr/share/applications/$LN2.desktop>/dev/null;then
         printf '\nStartupWMClass=$SWMC2'|tee -a /usr/share/applications/$LN2.desktop>/dev/null
@@ -86,7 +86,7 @@ install_appimage() {
         else
         sudo ln -fs /opt/$basename/$LN.desktop /usr/local/share/applications/$basename.desktop
     fi
-    sudo sed -i "s/Exec=$EXEC_OLD/Exec=$EXEC_NEW/g" /usr/local/share/applications/$basename.desktop
+    sudo sed -i "s/^Exec=$EXEC_OLD/Exec=$EXEC_NEW/g" /usr/local/share/applications/$basename.desktop
     sudo sed -i "s/Icon=$ICON_OLD/Icon=$ICON_NEW/g" /usr/local/share/applications/$basename.desktop
     if [ -n "$SWMC" ];then
         printf "\nStartupWMClass=$SWMC"|sudo tee -a /usr/local/share/applications/$basename.desktop>/dev/null
